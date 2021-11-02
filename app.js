0976fb5828901a5cceb5e539d277a7d0b438d775
ocardiel <ocardiel@users.noreply.github.com>
2021-11-02 18:40:33 +0100

    Pruebas con eslint

---
+++ app.js
@@ -0,0 +1,12 @@
+const nombre = "Matias";
+
+const persona = { nombre };
+
+console.log("persona: ", persona);
+
+const saludar = (fNombre) => {
+  console.log(`Hola! ¿que tal, ${fNombre}?`);
+};
+const nombre1 = "Pepito";
+
+const persona2 = { nombre: "Otra persona" };
